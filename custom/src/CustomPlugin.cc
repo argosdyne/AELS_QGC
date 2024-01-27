882d68f7cc0c897a5f4a0db155e3c4b61d8b04e2
Thanh Nguyen <thanhnguyen@kaist.ac.kr>
2024-01-28 01:53:25 +0900

    Add RTCM setting

--- custom/src/CustomPlugin.cc
+++ custom/src/CustomPlugin.cc
@@ -127,6 +127,7 @@ CustomPlugin::settingsPages()
         _addSettingsEntry(tr("Offline Maps"),"qrc:/qml/OfflineMap.qml",          "qrc:/res/waves.svg");
         _addSettingsEntry(tr("MAVLink"),     "qrc:/qml/MavlinkSettings.qml",     "qrc:/res/waves.svg");
         _addSettingsEntry(tr("Console"),     "qrc:/qml/QGroundControl/Controls/AppMessages.qml");
+        _addSettingsEntry(tr("RTCM"), "qrc:/custom/RTCMSettings.qml");
 #if defined(QT_DEBUG)
         //-- These are always present on Debug builds
         _addSettingsEntry(tr("Mock Link"),   "qrc:/qml/MockLink.qml");
